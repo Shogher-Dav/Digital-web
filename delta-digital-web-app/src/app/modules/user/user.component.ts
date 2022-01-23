928433fc84109f7a85987dc463adc298b75f9165
sh_davtyan <sh_davtyan@controlsystems.ru>
2022-01-23 14:50:52 +0400

    get current user name, surname, email and show

--- delta-digital-web-app/src/app/modules/user/user.component.ts
+++ delta-digital-web-app/src/app/modules/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-user',
@@ -9,10 +10,17 @@ import { Router } from '@angular/router';
 export class UserComponent implements OnInit {
 
   isInDevice = false;
+  currentUser: any;
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.getCurrentUser()
+    .subscribe(res => {
+      this.currentUser = res;
+    });
   }
 
   openDevice(event: any) {
